226a3d5fedec63744e683a28bca89575416b8630
germancordellat <germancordellat@gmail.com>
2026-01-04 15:11:57 +0100

    Agregar configuración de trazabilidad y mejorar el registro en el controlador de usuarios

--- src/main/java/com/prueba/msvc/users/controllers/UserController.java
+++ src/main/java/com/prueba/msvc/users/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.prueba.msvc.users.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,15 +26,19 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody User user) {
+        logger.info("Creating user with username: {}", user.getUsername());
         return ResponseEntity.ok(userService.save(user));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User user) {
+        logger.info("Updating user with id: {}", id);
         Optional<User> existingUser = userService.update(user, id);
         if (existingUser.isPresent()) {
             return ResponseEntity.status(HttpStatus.CREATED).body(existingUser.get());
@@ -43,20 +49,24 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        logger.info("Fetching user with id: {}", id);
         return userService.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
     @GetMapping("/username/{username}")
     public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        logger.info("Fetching user with username: {}", username);
         return userService.findByUsername(username).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
     
     @GetMapping
     public ResponseEntity<Iterable<User>> findAll() {
+        logger.info("Fetching all users");
         return ResponseEntity.ok(userService.findAll());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        logger.info("Deleting user with id: {}", id);
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
